0c599aadea5cd5669db98844975ffd488fc9bc82
Yanfei Zhang <yz1345@nyu.edu>
2015-06-09 17:37:25 +0400

    Plotted mass over velocity diagrams for specific points.

--- 02_NGC4549/massVsys=all/massVelocity.py
+++ 02_NGC4549/massVsys=all/massVelocity.py
@@ -1,20 +1,27 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-vSys = 596.937
+vSys = 679.104
 data = open("massvSys=" + str(vSys) + ".csv", "r")
 
 vlist = []
 mlist = []
 
 for line in data:
-	v = float(line.split(";")[0])
-	m = float(line.split(";")[4])
+	v = float(line.split()[0])
+	m = float(line.split()[4])
 	vlist.append(v)
 	mlist.append(m)
 
 data.close()
+x = np.arange(vlist[-1], vlist[0], 0.1)
+y = [10**6 for i in range(len(x))]
 plt.plot(vlist, mlist, 'bo')
-plt.plot()
-plt.show()
+plt.plot(x, y, c='red')
+plt.title("vSys=" + str(vSys))
+plt.xlabel("Velocity (km/s)")
+plt.ylabel("Mass (Mo)")
+
+#plt.show()
+plt.savefig("massVelocityvSys=" + str(vSys) + '.png')
 
